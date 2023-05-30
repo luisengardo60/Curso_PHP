ae6bb6d1bafb2c19f0f045c8025f8f6278890af9
luisengardo <luisengardo60@gmail.com>
2023-05-30 14:48:17 -0400

    first commit

---
+++ Java1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PROBANDO JAVASCRIPT</title>
+</head>
+<body>
+    <H2>EL PRIMERO</H2>
+
+    <button type="button"
+    onclick="document.getElementById('demo').innerHTML = Date()">
+    Tipeame y veras Fecha y Hora.</button>
+
+<p id="demo"></p>
+
+
+    </body>
+</html>
---
+++ Prueba.php
@@ -0,0 +1,6 @@
+<!DOCTYPE 	hmtl>
+<html>
+<body>
+	<?php echo "Mi primer PHP en el ambiente de Pruebas!";?>
+</body>
+</html>
